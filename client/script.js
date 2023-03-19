28595b45b4cec3efc0ba074d73bd7dba7d3faea0
Mastean12 <128087394+Mastean12@users.noreply.github.com>
2023-03-19 11:01:38 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handlesubmit = async (e) => {
 
     //fetch data from ther serever -> bot response
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://heiai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
